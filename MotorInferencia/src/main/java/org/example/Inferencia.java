f6c855a4c74c4a6ff1ab6afa9c77b4422f353d91
Juando26030 <juando02603@gmail.com>
2025-03-27 10:52:08 -0500

    añadir Tweety al pom para Fol

---
+++ MotorInferencia/src/main/java/org/example/Inferencia.java
@@ -0,0 +1,8 @@
+package org.example;
+
+
+public class Inferencia {
+    public void predicadosToConjuntiva() {
+        System.out.println("Predicados a Forma Conjuntiva");
+    }
+}
--- MotorInferencia/src/main/java/org/example/Main.java
+++ MotorInferencia/src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
 public class Main {
+    private final Inferencia inferencia = new Inferencia();
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public void cargarReglasBase(){
+        System.out.println("Reglas cargadas en predicados");
+    };
+
+    public void cargarHechos(){
+        System.out.println("Hechos cargados en predicados");
+    };
+
+    public void cargarPreguntas(){
+        System.out.println("Preguntas cargadas en predicados");
+    };
+
+    public void callConjuntiva(){
+
+        inferencia.predicadosToConjuntiva();
+    };
+
 }
